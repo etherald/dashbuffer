bb909b3d488aa2c1590fdf1a2924766cc65c9c0f
Etherald <web@etherald.com>
2014-07-17 18:54:03 -0400

    docs and comments

--- dashbuffer.el
+++ dashbuffer.el
@@ -1,5 +1,6 @@
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; dashbuffer.el
+;;; https://github.com/etherald/dashbuffer
 ;;; run elisp to create an informational buffer in your emacs
 
 ;;; to use, say like:
@@ -13,7 +14,7 @@
 (setq debug-on-error t)
 ;;; if auto update true and when-idle true repeate update after idle
 ;;; timeout
-;;; if auto update false andwhen-idle true update once after timeout
+;;; if auto update false and when-idle true update once after timeout
 ;;; if auto update true and when-idle false update always after interval
 ;;; if both false show info once, never update
 
@@ -23,12 +24,12 @@
   :group 'dashbuffer
   :type 'string)
 
-(defcustom dashbuffer-update-interval 30
+(defcustom dashbuffer-update-interval 10
   "Interval in seconds between dashbuffer updates."
   :group 'dashbuffer
   :type 'integer)
 
-(defcustom dashbuffer-idle-interval 3
+(defcustom dashbuffer-idle-interval 30
   "Interval in seconds for idle wait."
   :group 'dashbuffer
   :type 'integer)
@@ -38,7 +39,7 @@
   :group 'dashbuffer
   :type 'boolean)
 
-(defcustom dashbuffer-start-when-idle nil
+(defcustom dashbuffer-start-when-idle t
   "Whether to update the buffer only after Emacs has been idle for the specified time.
 Defaults to true. Otherwise the buffer will update after dashbuffer-update-interval has elapsed."
   :group 'dashbuffer
@@ -48,9 +49,10 @@ Defaults to true. Otherwise the buffer will update after dashbuffer-update-inter
 (defvar dashbuffer-timer nil)
 (defvar dashbuffer-itself nil)
 
+;;; entry point ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun dashbuffer ()
+  "Make a working dashbuffer appear in the Emacs."
   (interactive)
-;  (add-hook 'kill-buffer-hook 'dashbuffer-cleanup)
   (if (buffer-live-p  dashbuffer-itself)
       (pop-to-buffer dashbuffer-itself  nil t)
     (dashbuffer-create))
@@ -58,7 +60,8 @@ Defaults to true. Otherwise the buffer will update after dashbuffer-update-inter
       (run-with-idle-timer dashbuffer-idle-interval t 'dashbuffer-update)))
 
 (defun dashbuffer-update ()
-  (interactive)
+  "Update buffer with newest info."
+;;  (interactive)
   (with-local-quit
     ;;(undo-boundary)
     (save-selected-window
@@ -70,47 +73,56 @@ Defaults to true. Otherwise the buffer will update after dashbuffer-update-inter
           (dashbuffer-reset-timer)))))
 
 (defun dashbuffer-create ()
+  "Create the Dashbuffer."
   (setq dashbuffer-itself (pop-to-buffer dashbuffer-name))
   (view-buffer dashbuffer-itself)
   (buffer-disable-undo dashbuffer-itself)
   (set-window-dedicated-p (get-buffer-window dashbuffer-itself) t)
   (dashbuffer-update)
-  (fit-window-to-buffer (get-buffer-window dashbuffer-itself))
-  (shrink-window-if-larger-than-buffer (get-buffer-window dashbuffer-itself))
+  (fit-window-to-buffer (get-buffer-window dashbuffer-name))
+  (shrink-window-if-larger-than-buffer (get-buffer-window dashbuffer-name))
   )
 
 (defun dashbuffer-kill-buffer ()
+  "Kill the Dashbuffer."
   (kill-buffer dashbuffer-name))
 
 (defun dashbuffer-cleanup ()
+  "Cleanup on exit function."
   (dashbuffer-cancel-timer)
   (setq dashbuffer-itself nil)
   (setq dashbuffer-timer nil))
 
 (defun kill-dashbuffer ()
+  "Kill the Dashbuffer interactively."
   (interactive)
   (dashbuffer-kill-buffer))
 
 (defun bury-dashbuffer ()
+  "Bury Alive the Dashbuffer."
   (interactive)
   (quit-window nil (get-buffer-window dashbuffer-itself))
   (bury-buffer dashbuffer-itself))
 
 (defun dashbuffer-reset-timer ()
+  "Reset and restart dashbuffer's timer."
   (dashbuffer-cancel-timer)
   (setq dashbuffer-timer
         (run-at-time dashbuffer-update-interval nil 'dashbuffer-update)))
 
 (defun dashbuffer-cancel-timer ()
+  "Cancel the timer and set its holder to nil."
   (if (dashbuffer-timer-exists)
       (progn
         (cancel-timer dashbuffer-timer)
         (setq dashbuffer-timer nil))))
 
 (defun dashbuffer-timer-exists ()
+  "Is there Dashbuffer Timer now?"
   (memq dashbuffer-timer timer-list))
 
 (defun dashbuffer-write-line (str)
+  "Send some text to the dashbuffer."
   (set-buffer dashbuffer-name)
   (setq buffer-read-only nil)
   ;;(princ (format "%s\n" str) dashbuffer-itself)
@@ -119,6 +131,7 @@ Defaults to true. Otherwise the buffer will update after dashbuffer-update-inter
   (setq buffer-read-only t))
 
 (defun dashbuffer-write-content (buf)
+  "Write a complete new Dashbuffer."
   ;;(require 'calendar)
   (require 'solar)
   ;;(set-buffer buf)
